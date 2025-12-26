6dce3defb016b0c30120b2deb73a302e652221f1
sagarbabu <sagarsurendhrababu@gmail.com>
2025-12-26 11:57:46 +0530

    poster section done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React,{useState} from 'react'
 import './App.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+    const [title,setTitle] = useState();
+    const [description,setDescription] = useState(); 
+    const [poster,setPoster] = useState([]);
+
+    useEffect(() => {
+      const fetchPosters = async () => {
+        try{
+          const response = await axios.get('http://localhost:2800/api/posters');
+          setPoster(response.data);
+        }catch(err){
+          console.error('Error fetching posters:', err);
+        }
+      };
+      fetchPosters();
+    }, [poster]);
+
+    const handlePosterSubmit = async(e) => {
+      e.preventDefault();
+     try{
+      const response = await axios.post('http://localhost:2800/api/posters', {title,description});
+      setPoster([ response.data, ...poster]);     
+     }catch(err){
+      console.error('Error creating poster:', err);
+     }
+    } 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, World!</h1>
+      <form onSubmit={handlePosterSubmit}>
+        <input type="text" onChange={e => setTitle(e.target.value)} />
+        <input type="text" onChange={e => setDescription(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {poster && poster.map((p) => (
+          <li key={p._id}>{p.title}: {p.description}</li>
+        ))}
+      </ul>
     </>
   )
 }
